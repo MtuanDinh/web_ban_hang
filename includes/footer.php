a82862076ef10eb15cff4581525f26a607e4dd05
Tuan Anh <213182592+anh03012005@users.noreply.github.com>
2026-06-15 23:41:41 +0700

    footer and main

---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="container">
+  <ul class="footer-links">
+    <li>
+        <p><a href="#">iPhone Air</a> | <a href="#">iPhone 17</a> | <a href="#">iPhone 17 Pro</a></p>
+        <p><a href="#">iPhone 17 Pro Max</a> | <a href="#">iPhone 16 Pro Max</a></p>
+        <p><a href="#">iPhone 16</a> | <a href="#">iPhone cũ</a> | <a href="#">Macbook Neo</a></p>
+    </li>
+    
+    <li>
+        <p><a href="#">Điện thoại</a> | <a href="#">Điện thoại iPhone</a> | <a href="#">Xiaomi</a></p>
+        <p><a href="#">Điện thoại Samsung Galaxy</a> | <a href="#">Điện thoại OPPO</a></p>
+        <p><a href="#">OPPO Find X9s</a> | <a href="#">OPPO Find X9 Ultra</a></p>
+    </li>
+    
+    <li>
+        <p><a href="#">Phụ kiện</a> | <a href="#">Sạc</a> | <a href="#">Tai nghe</a> | <a href="#">Pin dự phòng</a></p>
+        <p><a href="#">Quạt tản nhiệt</a> | <a href="#">Sò lạnh</a> | <a href="#">Ốp lưng</a> | <a href="#">Cường lực</a></p>
+    </li>
+    
+</ul>
+<div class="footer-info">
+            Công ty Cổ phần Thương Mại Tổng Hợp HTV - GPĐKKD: 0108075931 cấp tại Sở KH & ĐT TP. Hà Nội. Địa chỉ văn phòng: 543 Đường Nguyễn Trãi, Phường Thanh Liệt, Thành phố Hà Nội, Việt Nam. Điện thoại: 024.7303.0119.
+
+
+        </div>
+
+        <div class="footer-badges">
+            <img src="assets/image/logo-footer.png" alt="Bộ công thương">
+            <img src="assets/image/logo-footer1.webp" alt="DMCA">
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -5,15 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://kit.fontawesome.com/da1a483940.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="assets/css/style_client.css">
-
+<link rel="stylesheet" href="assets/css/style_client.css?v=1">
 </head>
 <body>
    <?php 
     include "includes/header.php" ;
     include "includes/nav.php" ;
     ?>
-   
+    <?php
+    include "main.php";
+    ?>
+   <?php
+    include "includes/footer.php";
+   ?>
 </body>
 </html>
 
