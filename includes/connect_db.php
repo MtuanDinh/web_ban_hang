00e7595b3787f85c20a99488419337703216fd81
sunzn <137430365+MtuanDinh@users.noreply.github.com>
2026-06-15 15:50:47 +0700

    Thêm nội dung cho file connect_db.php

---
+++ includes/connect_db.php
@@ -0,0 +1,15 @@
+<?php 
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "phone_store";
+    $conn = "";
+    try{
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+        mysqli_set_charset($conn, "utf8mb4");
+    }
+    catch(mysqli_sql_exception $e){
+        die("Can't connect to database: {$e->getMessage()}");
+    }
+
+?>
