faaab969410ff84d9673920ca994da3e7b32da68
sunzn <137430365+MtuanDinh@users.noreply.github.com>
2026-06-21 18:10:22 +0700

    Thêm phần quản lý sản phẩm và dữ liệu mẫu

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -7,7 +7,7 @@
     <ul>
         <a href="/web_ban_hang/admin/index.php"><li>Tổng quan</li></a>
         <a href="#"><li>Quản lý đơn hàng</li></a>
-        <a href="#"><li>Quản lý sản phẩm</li></a>
+        <a href="/web_ban_hang/admin/products/list.php"><li>Quản lý sản phẩm</li></a>
         <a href="/web_ban_hang/admin/categories/list.php"><li>Quản lý danh mục</li></a>
         <a href="#"><li>Quản lý khách hàng</li></a>
     </ul>
